aac2ec8b764f2c4ce6abb63ca064fb5a5065d2d1
luomanqiang <luomanqiang@oasou.com>
2018-12-19 00:35:13 +0800

    test

---
+++ src/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 将以逗号分隔的字附串转换为数组
+ * @param string $str
+ * @return array
+ */
+function getArray($str) {
+    $str = explode(',', $str);
+    foreach ($str as $k => $v) {
+        if (!$v) {
+            unset($str[$k]);
+        }
+    }
+    return $str;
+}
